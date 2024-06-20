8c3e2d5411642ef7b9823709fbc70dec9ac4ed4e
XenoRx <franboise123@gmail.com>
2024-06-20 22:20:58 +0300

    added: acceleration, enemy movement

---
+++ scripts/game.gd
@@ -0,0 +1,10 @@
+extends Node2D
+
+#var speed = 200
+#
+#func _ready():
+	#randomize()
+
+#func _physics_process(delta):
+	#for area in $Areas.get_children()
+	#area.position.x -= speed * delta
--- scripts/player.gd
+++ scripts/player.gd
@@ -1,6 +1,7 @@
 extends CharacterBody2D
 
-
+const ACCELERATION = 1
+const MAX_SPEED = 200
 const SPEED = 70.0
 const JUMP_VELOCITY = -250.0
 
@@ -12,6 +13,7 @@ func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
+	velocity.x += ACCELERATION if velocity.x < MAX_SPEED else 0
 
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
@@ -19,10 +21,10 @@ func _physics_process(delta):
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+	#var direction = Input.get_axis("ui_left", "ui_right")
+	#if direction:
+		#velocity.x = direction * SPEED
+	#else:
+		#velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
